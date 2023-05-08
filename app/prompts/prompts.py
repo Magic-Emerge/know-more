f651c72e721f83fea90388b73a7e3c7fff904f2b
mazean <mazean@enn.cn>
2023-05-08 15:42:25 +0800

    fix code

--- app/prompts/prompts.py
+++ app/prompts/prompts.py
@@ -10,7 +10,7 @@ _template = """给定以下聊天记录和后续输入问题，将后续输入�
 """
 CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_template)
 
-prompt_template = """使用以下内容来回答最后的问题。 如果回答与话题无关的问题，如果你不知道答案，就回答你不知道，不要试图编造答案。
+prompt_template = """使用以下内容来回答最后的问题。如果你不知道答案，就回答你不知道，不要试图编造答案。
 {context}
 问题: {question}
 答案:
